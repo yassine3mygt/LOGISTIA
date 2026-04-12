8cf27a293f02a09a8f7b58cce8f4f48c6d37581d
Yassine Touil <Yassinemygt@gmail.com>
2026-04-12 23:30:35 +0100

    Auth Servce security

---
+++ auth-service/src/main/java/com/example/auth_service/security/JwtAuthFilter.java
@@ -0,0 +1,4 @@
+package com.example.auth_service.security;
+
+public class JwtAuthFilter {
+}
--- auth-service/src/main/java/com/example/auth_service/security/JwtUtils.java
+++ auth-service/src/main/java/com/example/auth_service/security/JwtUtils.java
@@ -1,31 +1,36 @@
 package com.example.auth_service.security;
 
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import io.jsonwebtoken.security.Keys;
 import java.security.Key;
+import io.jsonwebtoken.Jwts;
 import java.util.Date;
 
 @Component
 public class JwtUtils {
 
-    // Clé secrète pour signer le token
+
+    // (Clé secrète pour signer le token)
     private final String JWT_SECRET = "logistia_secret_key_very_long_string_123456789";
 
-    // Durée de validité : 24h en millisecondes
+    // (Validité du token : 24h en millisecondes)
     private final long JWT_EXPIRATION = 86400000;
 
-    // Générer un token
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
+    }
+
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .setExpiration(new Date(
+                        System.currentTimeMillis() + JWT_EXPIRATION))
                 .signWith(getSigningKey())
                 .compact();
     }
 
-    // Extraire l'email depuis le token
     public String getEmailFromToken(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getSigningKey())
@@ -35,21 +40,16 @@ public class JwtUtils {
                 .getSubject();
     }
 
-    // Valider le token
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder()
                     .setSigningKey(getSigningKey())
                     .build()
                     .parseClaimsJws(token);
-            return true;
+            return true;   // token is valid ✅
         } catch (Exception e) {
-            return false;
+            return false;  // token is invalid ❌
         }
     }
 
-    // Créer la clé de signature
-    private Key getSigningKey() {
-        return Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
-    }
 }
