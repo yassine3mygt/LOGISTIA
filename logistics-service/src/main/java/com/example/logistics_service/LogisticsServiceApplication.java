ef7e82525639d9994e1831dea2a69633bf99920e
Yassine Touil <Yassinemygt@gmail.com>
2026-04-26 00:06:32 +0100

    initialisation du logistics-service

---
+++ logistics-service/src/main/java/com/example/logistics_service/LogisticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.logistics_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsServiceApplication.class, args);
+	}
+
+}
